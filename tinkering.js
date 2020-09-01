8ec04b0c6617e53ab3403b85a4f8dce7725768b9
Rajdeep Ghatore <r.ghatore@gmail.com>
2020-09-01 21:13:28 +0000

    Tinkering file created to test various codes.

---
+++ tinkering.js
@@ -0,0 +1,51 @@
+/*
+function multiply(a, b) {
+return a*b;
+  return 2*5;
+
+}
+
+const result = multiply(2, 3);
+
+console.log(result);
+*/
+/*
+let example = "Hello, \n\twhat is this?"
+//console.log(example.length);
+console.log(example.charCodeAt(3));
+console.log(example[2],example[4]);
+*/
+/*
+let money = 100.50;
+let price = '100.50';
+
+if (money > price) {
+  console.log("You paid extra, here's your change.");
+} else if (money === price) {
+  console.log("You paid the exact amount, have a nice day!");
+} else {
+  console.log("That's not enough, you still owe me money.");
+} */
+
+let option = 7;
+
+switch (option) {
+  case 1:
+//    console.log("You selected option 1.");
+//    break;
+  case 2:
+//    console.log("You selected option 2.");
+//    break;
+  case 3:
+//    console.log("You selected option 3.");
+//    break;
+  case 4:
+//    console.log("You selected option 4.");
+//    break;
+  case 5:
+//    console.log("You selected option 5.");
+//    break;
+  case 6:
+    console.log("You selected option " + option + ".");
+    break; // technically, not needed
+}
