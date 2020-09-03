6233bab64fe6ecd9c3bc99137bbc0c1be236f051
Rajdeep Ghatore <r.ghatore@gmail.com>
2020-09-03 17:43:28 +0000

    testing out arrays and loops on arrays

---
+++ ice-cream.js
@@ -0,0 +1,11 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// activity code
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+//make sure to add space between operators
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+console.log(iceCreamFlavours.length);
